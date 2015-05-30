8ca8e6eb03362ba56714f7a24027abd0ab707cf7
Filip Procházka <filip@prochazka.su>
2015-05-30 11:04:08 +0200

    allowed methods

--- app/presenters/ApiModule/presenters/BasePresenter.php
+++ app/presenters/ApiModule/presenters/BasePresenter.php
@@ -9,6 +9,8 @@
 namespace App\ApiModule\Presenters;
 
 use Nette;
+use Nette\Http\IRequest;
+use Nette\Http\IResponse;
 use Tracy\Debugger;
 
 
@@ -19,16 +21,25 @@ use Tracy\Debugger;
 abstract class BasePresenter extends \App\BasePresenter
 {
 
-	/**
-	 * @persistent
-	 */
-	public $format = 'json';
+	const HTTP_HEADER_ALLOW = "Allow";
+	const HTTP_HEADER_AUTHENTICATION_SIMPLE = "X-Authentication-Simple";
 
 	/**
 	 * @var Nette\Application\Application
 	 * @inject
 	 */
 	public $application;
+
+	/**
+	 * @var string[]
+	 */
+	private static $actionMap = [
+		'read' => IRequest::GET,
+		'readAll' => IRequest::GET,
+		'create' => IRequest::POST,
+		'update' => IRequest::PUT,
+		'delete' => IRequest::DELETE,
+	];
 
 
 
@@ -38,6 +49,44 @@ abstract class BasePresenter extends \App\BasePresenter
 		$this->application->errorPresenter = 'Api:Error';
 		$this->application->catchExceptions = TRUE;
 		parent::startup();
+
+		if (!$this->isMethodAllowed($this->getAction())) {
+			$this->getHttpResponse()->addHeader(self::HTTP_HEADER_ALLOW, implode(", ", $this->getAllowedMethods()));
+			$this->error("Method '{$this->getAction()}' not allowed", IResponse::S405_METHOD_NOT_ALLOWED);
+		}
+	}
+
+
+
+	/**
+	 * Returns TRUE if given action is supported by current presenter.
+	 *
+	 * @param string $action
+	 * @return bool
+	 */
+	private function isMethodAllowed($action)
+	{
+		return $this->reflection->hasMethod($this->formatActionMethod($action))
+		|| $this->reflection->hasMethod($this->formatRenderMethod($action));
+	}
+
+
+
+	/**
+	 * Returns array of allowed methods by current presenter.
+	 *
+	 * @return string[]
+	 */
+	private function getAllowedMethods()
+	{
+		$allowedMethods = [];
+		foreach (self::$actionMap as $action => $method) {
+			if ($this->isMethodAllowed($action)) {
+				$allowedMethods[] = $method;
+			}
+		}
+
+		return array_unique($allowedMethods);
 	}
 
 }
