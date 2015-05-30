01d01644e6fa0b42b185de869eee874d666e38ab
Filip Procházka <filip@prochazka.su>
2015-05-30 10:51:10 +0200

    add modules and first api route

---
+++ app/presenters/BasePresenter.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This file is part of the Kdyby (http://www.kdyby.org)
+ * Copyright (c) 2008 Filip Procházka (filip@prochazka.su)
+ * For the full copyright and license information, please view the file license.txt that was distributed with this source code.
+ */
+
+namespace App;
+
+use Nette;
+
+
+
+/**
+ * @author Filip Procházka <filip@prochazka.su>
+ */
+abstract class BasePresenter extends Nette\Application\UI\Presenter
+{
+
+}
--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Api\Routers\RestRoute;
 use Nette,
 	Nette\Application\Routers\RouteList,
 	Nette\Application\Routers\Route;
@@ -13,10 +14,15 @@ class RouterFactory
 	/**
 	 * @return Nette\Application\IRouter
 	 */
-	public static function createRouter()
+	public function create()
 	{
 		$router = new RouteList();
-		$router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default');
+		$router[] = $api = new RouteList('Api');
+		$api[] = new RestRoute('/api/shows[.<format=json>]', 'Shows:');
+
+		$router[] = $front = new RouteList('Front');
+		$front[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default');
+
 		return $router;
 	}
 
